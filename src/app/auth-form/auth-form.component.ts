d81e83dee948a65b98aa88cc66326e22d6fbac65
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-05 17:07:57 +0530

    Using the platform agnostic Renderer

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,5 +1,6 @@
 import { 
   Component, 
+  Renderer,
   ChangeDetectorRef, 
   Output, 
   ViewChild, 
@@ -35,7 +36,10 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   @Output() 
   submitted: EventEmitter<User> = new EventEmitter<User>();
 
-  constructor(private cd: ChangeDetectorRef) { }
+  constructor(
+    private renderer: Renderer,
+    private cd: ChangeDetectorRef
+  ) { }
 
   ngAfterContentInit() {
     if (this.remember) {
@@ -47,9 +51,13 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.email.nativeElement.setAttribute('placeholder', 'Enter your email address');
-    this.email.nativeElement.classList.add('email');
-    this.email.nativeElement.focus();
+    // Platform safe code
+    this.renderer.setElementAttribute(this.email.nativeElement, 'placeholder', 'Enter your email address');
+    this.renderer.setElementClass(this.email.nativeElement, 'email', true);
+    this.renderer.invokeElementMethod(this.email.nativeElement, 'focus');
+    // this.email.nativeElement.setAttribute('placeholder', 'Enter your email address');
+    // this.email.nativeElement.classList.add('email');
+    // this.email.nativeElement.focus();
     if (this.message) {
       this.message.forEach((message) => {
         message.days = 30;
