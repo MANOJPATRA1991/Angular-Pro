c0ffadd1cbdd55e2d06e7d6bcc8c077329aa94b9
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-05 14:28:59 +0530

    Projecting and Binding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { AuthFormComponent } from './auth-form/auth-form.component';
+import { AuthFormModule } from './auth-form/auth-form.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AuthFormComponent ],
+  imports:      [ BrowserModule, FormsModule, AuthFormModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
