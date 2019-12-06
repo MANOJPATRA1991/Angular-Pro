109d9bfc395f4accd6867bbf4283a74462502f35
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-06 12:56:57 +0530

    Dynamic <ng-template> rendering with ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,18 +16,11 @@ import { User } from './auth-form/auth-form.interface';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements AfterContentInit {
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
   constructor(
     private resolver: ComponentFactoryResolver
   ) { }
 
   ngAfterContentInit() {
-    this.entry.createEmbeddedView(this.tmpl, {
-      $implicit: 'Manoj Patra',
-      location: 'India'
-    });
   }
 
   loginUser(user: User) {
