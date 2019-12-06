1a470fc65bfec4fe7f9bde651fd2f93290e6a2ba
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-06 14:39:33 +0530

    @HostListener and host object

--- src/app/credit-card.directive.ts
+++ src/app/credit-card.directive.ts
@@ -1,6 +1,7 @@
 import { 
   Directive, 
-  ElementRef  
+  ElementRef,
+  HostListener  
 } from '@angular/core';
 
 // We can't declare a template inside a directive
@@ -8,12 +9,24 @@ import {
   selector: '[appCreditCard]'
 })
 export class CreditCardDirective {
+  // The host is the element that we have bound the directive to
+  @HostListener('input', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    console.log(event);
+    const input = event.target as HTMLInputElement;
+    let trimmed = input.value.replace(/\s+/g, '');
+    if (trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
 
-  constructor(
-    // For talking to the DOM node via the directive
-    private element: ElementRef
-  ) { 
-    console.log(this.element);
+    // [1234, 5678, 1245, 1234]
+    let numbers = [];
+
+    for (let i = 0; i < trimmed.length; i += 4) {
+      numbers.push(trimmed.substr(i, 4));
+    }
+
+    input.value = numbers.join(' ');
   }
 
 }
