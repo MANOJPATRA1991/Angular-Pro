6916ffe55c8c4f17fd93a2e5b142e669b41ffb62
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-11 17:08:15 +0530

    Using the exportAs property with template refs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AuthFormModule } from './auth-form/auth-form.module';
 import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AuthFormModule ],
-  declarations: [ AppComponent, CreditCardDirective ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    AuthFormModule 
+    ],
+  declarations: [ 
+    AppComponent, 
+    CreditCardDirective, 
+    TooltipDirective 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
