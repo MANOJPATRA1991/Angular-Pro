117dfbf500fa551ec660cbeb50ab6e8c704e8977
MANOJPATRA1991 <patra.manoj0@gmail.com>
2020-02-09 23:42:43 +0530

    Routing: Custom preloading strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes, Route, PreloadingStrategy, PreloadAllModules } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuthFormModule } from './auth-form/auth-form.module';
@@ -12,12 +13,18 @@ import { FileSizePipe } from './file-size/file-size.pipe';
 import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
 import { MailModule } from './mail/mail.module';
 
+export class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data.preload ? fn() : of(null);
+  }
+}
+
 export const ROUTES: Routes = [
   // LAZY LOADING DASHBOARD MODULE
   {
     path: 'dashboard',
-    loadChildren: './dashboard/dashboard.module#DashboardModule',
-
+    data: { preload: true },
+    loadChildren: './dashboard/dashboard.module#DashboardModule'
   },
   {
     path: '**',
@@ -34,8 +41,12 @@ export const ROUTES: Routes = [
     StockInventoryModule,
     MailModule,
     RouterModule.forRoot(ROUTES, {
-      preloadingStrategy: PreloadAllModules
+      // preloadingStrategy: PreloadAllModules
+      preloadingStrategy: CustomPreload
     })
+  ],
+  providers: [
+    CustomPreload
   ],
   declarations: [ 
     AppComponent, 
