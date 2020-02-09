fb30364ffc332ab985ddd0e34561411cc6c4b23d
Manoj Kumar Patra <patra.manoj0@gmail.com>
2020-02-09 10:21:37 +0530

    Routing: Router outlet events

--- src/app/mail/components/mail-app/mail-app.component.ts
+++ src/app/mail/components/mail-app/mail-app.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mail-app',
-  templateUrl: './mail-app.component.html',
-  styleUrls: ['./mail-app.component.scss']
+	selector: 'app-mail-app',
+	templateUrl: './mail-app.component.html',
+	styleUrls: ['./mail-app.component.scss'],
 })
 export class MailAppComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	// These are not commonly used
 
+	onActivate(event) {
+		console.log('Activate', event);
+	}
+
+	onDeactivate(event) {
+		console.log('Deactivate', event);
+	}
 }
