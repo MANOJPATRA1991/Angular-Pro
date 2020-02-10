cc6746da419c1cef60a891aab2ccbd6c0151c995
MANOJPATRA1991 <patra.manoj0@gmail.com>
2020-02-10 07:29:05 +0530

    Testing: setup karma for testing

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    mime: { 'text/x-typescript': ['ts'] },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -20,12 +21,56 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    preprocessors: {
+      '*.js': ['sourcemap'],
+      '**/*.spec.ts': ['sourcemap', 'webpack']
+    },
+    reporters: ['spec', 'progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    files: [
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      { pattern: __dirname + '/**/*.spec.ts', watched: false }
+    ],
+    webpack: {
+      context: __dirname,
+      devtool: 'sourcemap',
+      module: {
+        rules: [
+          {
+            test: /\.html$/,
+            loaders: ['raw-loader']
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['raw-loader', 'sass-loader']
+          },
+          {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+          }
+        ]
+      },
+      plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.SourceMapDevToolPlugin({
+          filename: null,
+          test: /\.(ts|js)($|\?)/i
+        })
+      ],
+      resolve: {
+        extensions: ['.ts', '.js']
+      }
+    }
   });
 };
